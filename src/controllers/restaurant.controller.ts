234fa95ac7f5033096ca5fa7aad9873f542c4253
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-06-16 19:37:56 +0900

    42 lesson Serive model Login

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const restaurantController: T = {};
@@ -36,13 +36,18 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     }
 };
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async(req: Request, res: Response) => {
     try {
         console.log("processLogin");
-        res.send("Login processed successfully");
+        console.log("body: ", req.body);
+        const input: LoginInput = req.body;
+
+        const memberService = new MemberService();
+        const result = await memberService.processLogin(input);
+        res.send(result);
     } catch (err) {
         console.log("Error, processLogin", err);
-        res.status(500).send("Error occurred during login");
+        res.send(err);
     }
 };
 
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,9 +1,10 @@
-import { Member, MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import MemberModel from "../schema/Member.model";
 import { MemberType } from "../libs/enums/member.enum";
 import Errors from "../libs/Errors";
 import { HttpCode } from "../libs/Errors";
 import { Message } from "../libs/Errors";
+import * as bcryptjs from "bcryptjs";
 
 
 class MemberService {
@@ -11,7 +12,7 @@ class MemberService {
 
     constructor() {
         this.memberModel = MemberModel;
-
+ 
     }
 
     public async processSignup(input: MemberInput): Promise<Member>{
@@ -21,6 +22,9 @@ class MemberService {
         console.log("exist: ", exist);
         if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
 
+        const salt = await bcryptjs.genSalt();
+        input.memberPassword = await bcryptjs.hash(input.memberPassword, salt);
+
         try{
             const result = await this.memberModel.create(input);
             result.memberPassword = "";
@@ -29,6 +33,33 @@ class MemberService {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
     }
+
+    public async processLogin(input: LoginInput): Promise<Member> {
+        // Find the member by their nickname and include the password field
+        const member = await this.memberModel
+            .findOne({ memberNick: input.memberNick }, { memberNick: 1, memberPassword: 1 })
+            .exec();
+    
+        // If the member does not exist, throw a NOT_FOUND error
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_ERROR_NICK);
+    
+        // Compare the input password with the stored hashed password
+        const isMatch = await bcryptjs.compare(input.memberPassword, member.memberPassword);
+    
+        // If the passwords do not match, throw an UNAUTHORIZED error
+        if (!isMatch) throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    
+        // Retrieve the full member data excluding the _id field
+        const fullMemberData = await this.memberModel
+            .findOne({ _id: member._id })
+            .exec();
+    
+        // Return the full member data
+        return fullMemberData;
+    }
+    
+    
+    
 }
 
 export default MemberService;
