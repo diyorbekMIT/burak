69e3efaac7c76d27afc0e228e354b9220789add0
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-06-16 19:59:40 +0900

    Task I

--- src/train.ts
+++ src/train.ts
@@ -1,8 +1,25 @@
-function getDigits (input: string) {
-    const numbers: string[] = ["0","2","3","4","5","6","7","8","9"];
-    const arr: number[] = input.split("").filter(char => numbers.includes(char)).map(char => parseInt(char));
-    return arr;
+function majorityElement(nums: number[]): number {
+    let frequency: { [key: string]: number } = {};
     
-};
+    for (let num of nums) {
+        if (frequency[num]) {
+            frequency[num]++;
+        } else {
+            frequency[num] = 1;
+        }
+    }
+    
+    let maxElement: string = nums[0].toString();
+    let maxCount: number = 1;
+    
+    for (let num in frequency) {
+        if (frequency[num] > maxCount) {
+            maxElement = num;
+            maxCount = frequency[num];
+        }
+    }
+    
+    return parseInt(maxElement);
+}
 
-console.log(getDigits("MIT14"));
+console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4])); // Output: 4
