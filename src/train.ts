d297aec5ae2de20e14a8be8340c44a25bbff0c2d
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-09-01 20:53:35 +0900

    feat: TASK ZQ

--- src/train.ts
+++ src/train.ts
@@ -1,29 +1,20 @@
-function areArraysEqual(arr1: number[], arr2: number[]): boolean {
-    if (arr1.length !== arr2.length) return false;
+function findDuplicates(arr: number[]): number[] {
   
-    const countElements = (arr: number[]) => {
-      const countMap = new Map<number, number>();
-      for (const num of arr) {
-        countMap.set(num, (countMap.get(num) || 0) + 1);
-      }
-      return countMap;
-    };
-  
-    const countMap1 = countElements(arr1);
-    const countMap2 = countElements(arr2);
-  
-    if (countMap1.size !== countMap2.size) return false;
-  
-    for (const [key, value] of countMap1) {
-      if (countMap2.get(key) !== value) {
-        return false;
-      }
-    }
-  
-    return true;
+  const countMap = new Map<number, number>();
+  const result: number[] = [];
+
+  for (const num of arr) {
+    countMap.set(num, (countMap.get(num) || 0) + 1);
   }
-  
-  console.log(areArraysEqual([1, 2, 3], [3, 1, 2])); 
-  console.log(areArraysEqual([1, 2, 3], [3, 1, 2, 1])); 
-  console.log(areArraysEqual([1, 2, 3], [4, 1, 2])); 
-  
+
+  for (const [key, value] of countMap) {
+    if (value === 2) {
+      result.push(key);
+    }
+  }
+
+  return result;
+}
+
+
+console.log(findDuplicates([1, 2, 3,2, 4, 5, 4, 3, 4])); 
