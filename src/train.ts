4536052f1c2ce8cf5b83f36ffe4b15ae22a112a8
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-09-01 16:25:50 +0900

    feat: TASK Zp

--- src/train.ts
+++ src/train.ts
@@ -1,23 +1,29 @@
-function areParenthesesBalanced(input: string): boolean {
-  let balance = 0;
-
-  for (let char of input) {
-      if (char === "(") {
-          balance++;
-      } else if (char === ")") {
-          balance--;
-      }
-
-      
-      if (balance < 0) {
-          return false;
-      }
-  }
-
+function areArraysEqual(arr1: number[], arr2: number[]): boolean {
+    if (arr1.length !== arr2.length) return false;
   
-  return balance === 0;
-}
-
-
-console.log(areParenthesesBalanced("string()ichida(qavslar)soni()balansda")); 
-
+    const countElements = (arr: number[]) => {
+      const countMap = new Map<number, number>();
+      for (const num of arr) {
+        countMap.set(num, (countMap.get(num) || 0) + 1);
+      }
+      return countMap;
+    };
+  
+    const countMap1 = countElements(arr1);
+    const countMap2 = countElements(arr2);
+  
+    if (countMap1.size !== countMap2.size) return false;
+  
+    for (const [key, value] of countMap1) {
+      if (countMap2.get(key) !== value) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  console.log(areArraysEqual([1, 2, 3], [3, 1, 2])); 
+  console.log(areArraysEqual([1, 2, 3], [3, 1, 2, 1])); 
+  console.log(areArraysEqual([1, 2, 3], [4, 1, 2])); 
+  
