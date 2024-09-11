8e0b01d9ff6b941548205046fbf168277dc31fb3
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-09-11 21:06:17 +0900

    feat: TASK ZT

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,14 @@
-function singleNumber(nums: number[]): number {
-  return nums.reduce((acc, num) => acc ^ num, 0);
+function firstUniqueCharIndex(s: string): number {
+  const charCount: Map<string, number> = new Map();
+
+  for (const char of s) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (charCount.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
 }
-console.log(singleNumber([4, 2, 1, 2, 1])); 
+console.log(firstUniqueCharIndex("stamp")); 
